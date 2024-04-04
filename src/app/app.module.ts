a494a47df9e850416694b0200d79c0749f8620e8
stepheeeennnnnnnn <123538443+stepheeeennnnnnnn@users.noreply.github.com>
2024-04-04 23:32:19 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { BookDetailComponent } from './components/book-detail/book-detail.component';
+import { BooksListComponent } from './components/books-list/books-list.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatChipsModule} from '@angular/material/chips';
+
+
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
+
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+import {MatBadgeModule} from '@angular/material/badge';
+
+
+import {MatRadioModule} from '@angular/material/radio';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddBookComponent,
+    BookDetailComponent,
+    BooksListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatIconModule,
+    MatChipsModule,
+
+    MatDividerModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatBadgeModule,
+    MatRadioModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
